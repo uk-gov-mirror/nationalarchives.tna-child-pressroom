87ca93f526c183ee9cc010b72b27f32a7f178b79
karlkern <karl.kern@nationalarchives.gov.uk>
2026-03-24 10:53:28 +0000

    change header

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function tnatheme_globals() {
     $tnatheme['ischildsite'] = 1;
     $tnatheme['childsitename'] = 'Press room';
     $headers = apache_request_headers();
-    if ( isset($_SERVER['HTTP_X_NGINX_PROXY']) && isset($headers['X_HOST_TYPE']) && $headers['X_HOST_TYPE'] == 'public' ) {
+    if ( isset($_SERVER['HTTP_X_NGINX_PROXY']) && isset($headers['X-Host-Type']) && $headers['X-Host-Type'] == 'public' ) {
         $tnatheme['subsitepath'] = '/about/press-room';
         /* array containing the path to the root with trailing slash - example below */
         $tnatheme['subsitepatharr'] = array(
